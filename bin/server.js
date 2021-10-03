064e1132f0ebefe65a9fb2f9874b5c3b05d5b834
sergiuszorgo <67199252+sergiuszorgo@users.noreply.github.com>
2021-10-03 19:35:56 +0300

    routes add

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,14 @@
 const app = require('../app')
+const dbase = require('../dbase/index')
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`)
-})
+dbase
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`)
+    })
+  })
+  .catch(error => {
+    console.log(`Server not running! Error: ${error.message}`)
+  })
